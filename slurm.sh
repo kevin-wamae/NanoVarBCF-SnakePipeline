235beaf9b16796fa2e79651de0834237b56025bd
Kevin Wamae <wamaekevin@gmail.com>
2024-05-03 12:26:54 +0300

    include option to run in conda and singularity

--- slurm.sh
+++ slurm.sh
@@ -8,20 +8,19 @@
 #SBATCH --partition=longrun
 #SBATCH --time=04:00:00
 #SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=100G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=10G
 #SBATCH -o job.%j.out
 #SBATCH -e job.%j.err
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=<>       # replace with your email address
+#SBATCH --mail-user=kkariuki@kemri-wellcome.org      # replace with your email address
 
 # ********************************************************
 # activate conda environment
 # ********************************************************
 
 source "${HOME}/miniforge3/etc/profile.d/conda.sh"
-conda activate snakemakeVersion7
+conda activate snakemake8
 
 # ********************************************************
 #--- snakemake commands ---
@@ -31,9 +30,9 @@ snakemake --unlock
 snakemake \
 	--use-conda \
 	--conda-frontend mamba \
-	--use-singularity \
-	--cores 8 \
-	--jobs 4 \
+	--software-deployment-method conda \
+	--cores 4 \
+	--jobs 1 \
 	--rerun-incomplete
 
 # ********************************************************
